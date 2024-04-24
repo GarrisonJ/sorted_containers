10497511b03df6b91d7f20741d5c94190cf322cd
Garrison Jensen <garrison.jensen@gmail.com>
2024-04-24 15:34:10 -0700

    Remove unused error class

--- lib/sorted_containers.rb
+++ lib/sorted_containers.rb
@@ -4,7 +4,3 @@ require_relative "sorted_containers/version"
 require_relative "sorted_containers/sorted_array"
 require_relative "sorted_containers/sorted_set"
 require_relative "sorted_containers/sorted_hash"
-
-module SortedContainers
-  class Error < StandardError; end
-end
